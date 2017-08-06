eaf9051683426a8ad7e07cfa5aab719741a3a022
gkuczera <gkuczera@gmail.com>
2017-08-06 22:24:42 +0200

    [single-threaded-logic] Added counter core logic

--- src/main/java/pl/gkuczera/files_counter/Application.java
+++ src/main/java/pl/gkuczera/files_counter/Application.java
@@ -1,9 +1,36 @@
 package pl.gkuczera.files_counter;
 
-public class Application 
-{
-    public static void main(String[] args)    
-    {
-    	
-    }
+import java.io.File;
+
+import pl.gkuczera.files_counter.core.FilesCounter;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		if (args.length < 1) {
+			System.out.println("The path was not provided");
+			return;
+		}
+		
+		String providedDirectory = args[0];
+		
+		if (providedDirectory == null || providedDirectory.isEmpty()) {
+			System.out.println("The provided path is empty or null");
+			return;			
+		}
+		
+		
+		File directory = new File(providedDirectory); 
+		
+		if (!directory.isDirectory()) {
+			System.out.format("The provided path is not a directory (%s)", directory.getAbsolutePath());
+			return;						
+		}
+		
+		FilesCounter counter = new FilesCounter(directory);
+		counter.setLogging(true);
+		counter.count();
+	}
+	
 }
---
+++ src/main/java/pl/gkuczera/files_counter/core/FilesCounter.java
@@ -0,0 +1,68 @@
+package pl.gkuczera.files_counter.core;
+
+import java.io.File;
+
+public class FilesCounter {
+	
+	private boolean logging;
+	
+	private File rootDirectory;
+
+	public FilesCounter(String rootDirectoryPath) {		
+		initialize(rootDirectoryPath);
+	}
+	
+	public FilesCounter(File rootDirectory) {				
+		initialize(rootDirectory);
+	}	
+	
+	public boolean isLogging() {
+		return logging;
+	}
+
+	public void setLogging(boolean logging) {
+		this.logging = logging;
+	}
+	
+	public void count() {
+		long filesCount = count(rootDirectory);
+		
+		System.out.format("Files number in the %s directory: %s", rootDirectory.getAbsolutePath(), filesCount);		
+	}	
+	
+	private long count(File directory) {
+		long filesCount = 0;
+		
+		try {
+			File[] files = directory.listFiles();					
+			
+			for (File file : files) {
+				if (file.isDirectory()) {
+					filesCount += count(file);
+				} else {
+					++filesCount;
+				}
+			}					
+		} catch (Exception exception) {
+			System.out.format("The files inside the %s directory could not be counted (%s)%n", directory.getAbsolutePath(), exception.getMessage());
+		}
+		
+		return filesCount;
+	}
+	
+	private void initialize(String rootDirectoryPath) {		
+		if (rootDirectoryPath == null || rootDirectoryPath.isEmpty()) {
+			throw new IllegalArgumentException("The provided argument is not a valid directory path");
+		}
+		
+		initialize(new File(rootDirectoryPath));
+	}		
+	
+	private void initialize(File rootDirectory) {
+		if (rootDirectory == null || !rootDirectory.isDirectory()) {
+			throw new IllegalArgumentException("The provided argument is not a valid directory");
+		}
+		
+		this.rootDirectory = rootDirectory;
+	}
+}
